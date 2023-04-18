3c944230918459f177e08afb754534e919733c9f
leemingue <lmg3864@naver.com>
2023-04-18 12:07:58 +0900

    23.04.18

---
+++ 23.04/23.04.18/교수님 실습/function.js
@@ -0,0 +1,19 @@
+function myfunc(n) {
+    console.log(n)
+}
+myfunc(4)
+
+const myfunc2 = function(n) {
+    console.log(n)
+}
+
+myfunc2(5)
+
+const myfunc3 = function factorial(n) {
+    console.log(n)
+    if (n<=1) {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+console.log(myfunc3(3))
---
+++ 23.04/23.04.18/교수님 실습/string.js
@@ -0,0 +1,3 @@
+const s1 = '안녕 \n 하세요'
+
+console.log(s1)
